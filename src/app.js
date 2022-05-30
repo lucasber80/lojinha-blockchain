4b0e6a3e9043eeaf3b32ca32bafcc8b2be061815
lucas <lucasber80@hotmail.com>
2022-05-30 17:17:36 -0300

    quase terminando

--- src/app.js
+++ src/app.js
@@ -10,7 +10,40 @@ app.set('view engine', 'handlebars');
 app.set("views", "./views");
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home',{
+        itens: [
+  {
+    price:1,
+    image:"/images/headset.jpg",
+    title:"headset gamer logitech"
+  },
+  {
+    price:2,
+    image:"/images/processador.jpg",
+    title:"Processador intel core"
+  },
+  {
+    price:1,
+    image:"/images/cadeira.jpg",
+    title:"Cadeira Gamer"
+  },
+  {
+    price:3,
+    image:"/images/xbox.jpg",
+    title:"Xbox Series S"
+  },
+  {
+    price:2,
+    image:"/images/tv.jpg",
+    title:"Televisão 45 polegadas"
+  },
+  {
+    price:6,
+    image:"/images/ps5.jpg",
+    title:"Video game playstation 5"
+  }
+]
+    });
 });
 
 app.listen(3000);
--- src/config.js
+++ src/config.js
@@ -1,20 +1,7 @@
 let endereco = "0x19768afe4981b162d7a851418a16eb00f85c0581";
-let lojas = [
-  "0xD0553fdeA1ABE8b59175d77112c40b3E19054137",
-  "0xb5546122495A64843D3ae27752c567D32f719CD0",
-  "0x78F281f168584C8cf416d4da0b197711BCd34CEA",
-]
+let loja = "0xD0553fdeA1ABE8b59175d77112c40b3E19054137"
 
-let itens = [
-  {
-    price:1,
-    title:"headset gamer logitech"
-  },
-  {
-    price:2,
-    title:"Processador intal"
-  }
-]
+
 
 let abi = [
     {
